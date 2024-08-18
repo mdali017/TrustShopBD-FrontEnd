37bb4e5abb8a8bc5bbe2b9cac0d918ea32de7340
mohabbaticicle <mohabbatali@iciclecorporation.com>
2024-08-18 23:57:02 +0600

    TrustShopBD Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-[100vh]">
+        <h1 className="text-xl font-bold">Welcome To TrustShopBD</h1>
+        <h1>I am Working As Soon As Posible</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
